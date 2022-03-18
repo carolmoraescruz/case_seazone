b75113f481f6d3d832e2a37310f014d9183392fa
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-17 22:20:58 -0300

    imp: starting main structure

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
 
 def main():
-    pass
+    df_listings = pd.read_csv('data/raw/listings-challenge.csv')
+    df_daily_revenue = pd.read_csv('data/raw/daily_revenue.csv')
 
+    print(df_listings.shape)
+    print(df_daily_revenue.shape)
 
 if __name__ == '__main__':
     main()
