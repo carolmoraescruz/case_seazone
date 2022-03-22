4896e966eaaa40b92487e0facbe6d46940ad9027
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-22 20:45:22 -0300

    imp: answer question 01

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import numpy as np
-import seaborn as sns
+
+from src.data.make_dataset import load_data
+from src.reports.reports import (
+    answer_first_question,
+    print_reservation_advance_quantiles,
+)
+from src.visualization.visualize import (
+    plot_hist_reservation_advance,
+    plot_revenue_per_date,
+)
 
 
 def main():
-    df_listings = pd.read_csv("data/raw/listings-challenge.csv")
-    df_daily_revenue = pd.read_csv("data/raw/daily_revenue.csv")
+    """Main function"""
 
-    print(df_listings.shape)
-    print(df_daily_revenue.shape)
+    df_listings, df_daily_revenue = load_data()
+
+    # Question 01
+    answer_first_question(df_listings, df_daily_revenue)
+
+    # Complementary Data Analysis
+    plot_revenue_per_date(df_daily_revenue)
+    plot_hist_reservation_advance(df_daily_revenue)
+    print_reservation_advance_quantiles(df_daily_revenue)
 
 
 if __name__ == "__main__":
