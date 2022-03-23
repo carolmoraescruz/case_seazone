29b2eb80373da87e4330e6cf65bdc1bbf2613830
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-22 23:24:41 -0300

    imp: create pickle functions

--- src/commons.py
+++ src/commons.py
@@ -1,5 +1,16 @@
 import holidays
 import pandas as pd
+import pickle
+
+WEEK_DAY_ORDER = {
+    0: "Mon",
+    1: "Tue",
+    2: "Wed",
+    3: "Thu",
+    4: "Fri",
+    5: "Sat",
+    6: "Sun",
+}
 
 BRAZILIAN_HOLIDAYS = holidays.Brazil()
 
@@ -18,3 +29,14 @@ def transform_dataframe(transformer, dataframe):
         columns=dataframe.columns,
         index=dataframe.index,
     )
+
+
+def dump_pickle(variable, path):
+    with open(path, "wb") as f:
+        pickle.dump(variable, f)
+
+
+def load_pickle(path):
+    with open(path, "rb") as f:
+        variable = pickle.load(f)
+    return variable
