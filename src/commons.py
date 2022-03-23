d1fb6e69b9676ac013cae814c14feba8eaa635bd
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-22 23:03:57 -0300

    imp: add transform_dataframe to commons

--- src/commons.py
+++ src/commons.py
@@ -1,7 +1,20 @@
 import holidays
+import pandas as pd
 
 BRAZILIAN_HOLIDAYS = holidays.Brazil()
 
 
 def is_holiday(date):
     return int(date in BRAZILIAN_HOLIDAYS)
+
+
+def transform_dataframe(transformer, dataframe):
+    """Applies sklearn transform in pandas dataframe and
+    converts the result in pandas dataframe
+    """
+
+    return pd.DataFrame(
+        transformer.transform(dataframe),
+        columns=dataframe.columns,
+        index=dataframe.index,
+    )
