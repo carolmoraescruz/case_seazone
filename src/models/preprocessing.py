982e9d3b230ee8614f71dfd02570638e2cd8c522
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-22 23:06:24 -0300

    imp: add preprocess revenue model steps to preprocessing.py

--- src/models/preprocessing.py
+++ src/models/preprocessing.py
@@ -2,6 +2,10 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from src.commons import transform_dataframe
+
 
 def one_hot_encode_column(dataframe, column_name):
     return pd.concat(
@@ -11,3 +15,25 @@ def one_hot_encode_column(dataframe, column_name):
         ],
         axis=1,
     ).drop(columns=column_name)
+
+
+def fit_preprocess_revenue_model(X_train, y_train=None):
+    """ """
+    imputer = SimpleImputer(strategy="mean")
+    imputer.fit(X_train)
+    X_train = imputer.transform(X_train)
+
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+
+    return imputer, scaler
+
+
+def preprocess_revenue_model(X, preprocess):
+    """ """
+
+    for p in preprocess:
+        X = transform_dataframe(p, X)
+
+    return X
