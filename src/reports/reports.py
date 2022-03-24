0054e23703fc327c8df5f391219972c033eae914
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-24 17:11:09 -0300

    imp: add functions to report answers of questions 01 and 02

--- src/reports/reports.py
+++ src/reports/reports.py
@@ -3,8 +3,14 @@
 import pandas as pd
 import numpy as np
 import holidays
-from src.models.preprocessing import one_hot_encode_column, preprocess_revenue_model
-from src.commons import WEEK_DAY_ORDER, load_pickle
+from src import FEATURES_PRICE_MODEL_Q1
+from src.data.make_dataset import (
+    make_predict_dataset_price_q1,
+    make_predict_dataset_revenue_q1,
+)
+from src.models.preprocessing import one_hot_encode_column, preprocess_transform
+from src.commons import WEEK_DAY_ORDER, is_holiday, load_pickle, transform_dataframe
+from src.models.train_model import train_revenue_model_q2, train_price_model_q1
 
 
 def print_reservation_advance_quantiles(df_daily_revenue):
@@ -44,68 +50,50 @@ def print_reservation_advance_quantiles(df_daily_revenue):
     )
 
 
-def answer_first_question(df_listings, df_daily_revenue):
-    data = pd.merge(
-        df_daily_revenue,
-        df_listings,
-        left_on="listing",
-        right_on="Código",
-        how="left",
-    )
-
-    temp = data[
-        (data["Localização"] == "JUR")
-        & (data["Categoria"] == "MASTER1Q")
-        & (data["date"].dt.month == 3)
-    ]
-
-    avg_last_offered_price_M1Q = temp[temp["last_offered_price"] > 0][
-        "last_offered_price"
-    ].mean()
-
-    temp = data[
-        (data["Localização"] == "JUR")
-        & (data["Categoria"] == "MASTER3Q")
-        & (data["date"].dt.month == 3)
-    ]
-    avg_last_offered_price_M3Q = temp[temp["last_offered_price"] > 0][
-        "last_offered_price"
-    ].mean()
-
-    avg_last_offered_price_M2Q = np.mean(
-        [avg_last_offered_price_M1Q, avg_last_offered_price_M3Q]
-    )
-
-    temp = data[
-        (data["Localização"] == "JUR")
-        & (data["Categoria"] == "MASTER1Q")
-        & (data["date"].dt.month == 3)
-    ]
-
-    avg_revenue_M1Q = temp[temp["revenue"] > 0]["revenue"].sum()
-
-    temp = data[
-        (data["Localização"] == "JUR")
-        & (data["Categoria"] == "MASTER3Q")
-        & (data["date"].dt.month == 3)
-    ]
-    avg_revenue_M3Q = temp[temp["revenue"] > 0]["revenue"].sum()
-
-    avg_revenue_M2Q = np.mean([avg_revenue_M1Q, avg_revenue_M3Q])
-
+def header_q1():
     print(
-        "\n{}\nWhat is the expected price and revenue for a listing tagged as JUR MASTER2Q in March?".format(
+        "\n{}\n1)What is the expected price and revenue for a listing tagged as JUR MASTER2Q in March?".format(
             89 * "*"
         )
     )
-    print("Expected price R$: {:.2f}".format(avg_last_offered_price_M2Q))
-    print("Expected revenue R$: {:.2f}".format(avg_revenue_M2Q))
+
+
+def header_q2():
+    print("\n{}\n2)What is Seazone's expected revenue for 2022?".format(89 * "*"))
+
+
+def answer_first_question():
+    """ """
+
+    # Price
+
+    data_pred_price = make_predict_dataset_price_q1()
+
+    price_preprocessor = load_pickle("models/preprocessor_price_model_q1.pickle")
+
+    price_model = load_pickle("models/regressor_price_model_q1.pickle")
+
+    X_pred = preprocess_transform(data_pred_price, price_preprocessor)
+
+    print("Modeled price R$: {:.2f}".format(price_model.predict(X_pred).mean()))
+
+    # Revenue
+    data_pred_revenue = make_predict_dataset_revenue_q1()
+
+    revenue_preprocessor = load_pickle("models/preprocessor_revenue_model_q1.pickle")
+
+    revenue_model = load_pickle("models/regressor_revenue_model_q1.pickle")
+
+    X_pred = preprocess_transform(data_pred_revenue, revenue_preprocessor)
+
+    print("Modeled revenue R$: {:.2f}".format(revenue_model.predict(X_pred).sum()))
 
 
 def answer_second_question():
+
     data_pred = pd.DataFrame()
     data_pred["date"] = pd.date_range(
-        start=pd.to_datetime("2022-03-16"), end=pd.to_datetime("2022-12-31")
+        start=pd.to_datetime("2022-01-01"), end=pd.to_datetime("2022-12-31")
     )
 
     data_pred["year"] = data_pred["date"].dt.year
@@ -114,19 +102,16 @@ def answer_second_question():
 
     data_pred["day_of_week"] = data_pred["date"].dt.dayofweek.replace(WEEK_DAY_ORDER)
 
-    brazilian_holidays = holidays.Brazil()
-
-    def is_holiday(date):
-        return int(date in brazilian_holidays)
-
     data_pred["holiday"] = data_pred["date"].apply(is_holiday)
     data_pred = one_hot_encode_column(data_pred, "day_of_week")
 
     data_pred = data_pred.drop(columns="date")
 
-    preprocessor = load_pickle("models/preprocessor_revenue_model.pickle")
-    model = load_pickle("models/regressor_revenue_model.pickle")
+    preprocessor = load_pickle("models/preprocessor_revenue_model_q2.pickle")
+    model = load_pickle("models/regressor_revenue_model_q2.pickle")
 
-    X_pred = preprocess_revenue_model(data_pred, preprocessor)
+    X_pred = preprocess_transform(data_pred, preprocessor)
 
-    model.predict(X_pred).sum()
+    valor_2022 = model.predict(X_pred).sum()
+
+    print("Expected revenue for 2022 R$: {:.2f}".format(valor_2022))
