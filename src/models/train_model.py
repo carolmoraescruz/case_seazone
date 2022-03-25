5424f01420deeab1e275ecc444aca24e93e877ae
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-25 16:23:34 -0300

    imp: training model to answer the third question

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,11 +5,13 @@ from sklearn.neural_network import MLPRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error as mae
 from src.features.build_features import (
+    build_features_reservations_model_q3,
     build_features_revenue_model_q1,
     build_features_revenue_model_q2,
     build_features_price_model_q1,
 )
 from src.models.preprocessing import (
+    fit_preprocess_reservations_model_q3,
     fit_preprocess_revenue_model_q2,
     preprocess_transform,
 )
@@ -100,5 +102,34 @@ def train_revenue_model_q2(df_listings, df_daily_revenue):
     dump_pickle(preprocessor, "models/preprocessor_revenue_model_q2.pickle")
 
     dump_pickle(model, "models/regressor_revenue_model_q2.pickle")
+
+    print("MAE(teste) = {:.2f}".format(score))
+
+
+def train_reservations_model_q3(df_daily_revenue):
+
+    print("Training reservations model - Q3")
+
+    X, y = build_features_reservations_model_q3(df_daily_revenue)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42
+    )
+
+    preprocessor = fit_preprocess_reservations_model_q3(X_train)
+
+    X_train = preprocess_transform(X_train, preprocessor)
+
+    X_test = preprocess_transform(X_test, preprocessor)
+
+    model = XGBRegressor(max_depth=6, n_estimators=100, reg_alpha=0.5).fit(
+        X_train, y_train
+    )
+
+    score = mae(y_test, model.predict(X_test))
+
+    dump_pickle(preprocessor, "models/preprocessor_reservations_model_q3.pickle")
+
+    dump_pickle(model, "models/regressor_reservations_model_q3.pickle")
 
     print("MAE(teste) = {:.2f}".format(score))
