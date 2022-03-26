7816952d3dc9ffd27e404ff36e4dcd88774a2ead
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-25 21:16:24 -0300

    imp: format graph appearance and add graph for model of question 02

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,14 +5,21 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from src.commons import WEEK_DAY_ORDER, is_holiday, load_pickle
+
+from src.features.build_features import build_features_revenue_model_q2
+from src.models.preprocessing import one_hot_encode_column, preprocess_transform
 
 
 def plot_revenue_per_date(df_daily_revenue):
     path = "reports/figures/revenue_per_date.png"
     temp = df_daily_revenue.groupby("date")[["revenue"]].mean().reset_index()
 
+    plt.style.use("seaborn")
     sns.lineplot(data=temp, x="date", y="revenue")
     plt.xticks(rotation=45)
+    plt.xlabel("Date")
+    plt.ylabel("Revenue (R$)")
     plt.tight_layout()
     plt.savefig(path)
     plt.close()
@@ -24,9 +31,61 @@ def plot_revenue_per_date(df_daily_revenue):
 def plot_hist_reservation_advance(df_daily_revenue):
     path = "reports/figures/histogram_reservation_advance.png"
 
+    plt.style.use("seaborn")
     plt.hist(df_daily_revenue["reservation_advance_days"].dropna(), bins=100)
+    plt.xlabel("Reservation advance (days)")
+    plt.ylabel("Number of reservations")
     plt.tight_layout()
-    plt.savefig("reports/figures/histogram_reservation_advance.png")
+    plt.savefig(path)
+    plt.close()
+
+    print("\n{}".format(89 * "*"))
+    print("Exporting graph histogram_reservation_advance to path: " + path)
+
+
+def plot_real_pred_data(df_listings, df_daily_revenue):
+
+    X, y = build_features_revenue_model_q2(df_listings, df_daily_revenue)
+
+    X["date"] = X.apply(
+        lambda x: pd.to_datetime(
+            str(int(x["year"])) + "-" + str(int(x["month"])) + "-" + str(int(x["day"]))
+        ),
+        axis=1,
+    )
+
+    data_pred = pd.DataFrame()
+    data_pred["date"] = pd.date_range(start=X["date"].min(), end=X["date"].max())
+
+    data_pred["year"] = data_pred["date"].dt.year
+    data_pred["month"] = data_pred["date"].dt.month
+    data_pred["day"] = data_pred["date"].dt.day
+
+    data_pred["day_of_week"] = data_pred["date"].dt.dayofweek.replace(WEEK_DAY_ORDER)
+
+    data_pred["holiday"] = data_pred["date"].apply(is_holiday)
+    data_pred = one_hot_encode_column(data_pred, "day_of_week")
+
+    data_pred = data_pred.drop(columns="date")
+
+    preprocessor = load_pickle("models/preprocessor_revenue_model_q2.pickle")
+    model = load_pickle("models/regressor_revenue_model_q2.pickle")
+
+    X_pred = preprocess_transform(data_pred, preprocessor)
+
+    y_pred = model.predict(X_pred)
+
+    path = "reports/figures/real_versus_predicted_revenue.png"
+
+    plt.style.use("seaborn")
+    plt.plot(X["date"], y, label="Real revenue")
+    plt.plot(X["date"], y_pred, label="Predicted revenue")
+    plt.xticks(rotation=45)
+    plt.xlabel("Date")
+    plt.ylabel("Revenue (R$)")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(path)
     plt.close()
 
     print("\n{}".format(89 * "*"))
