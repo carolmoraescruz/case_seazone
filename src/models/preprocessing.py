1ec3c5457a6f2224c04c633f2d503700f3c926d4
Caroline Moraes da Cruz <carolmoraescruz@gmail.com>
2022-03-26 11:29:59 -0300

    imp: adding docstrings to the prepeocessing functions

--- src/models/preprocessing.py
+++ src/models/preprocessing.py
@@ -7,8 +7,22 @@ from sklearn.preprocessing import MinMaxScaler
 from src.commons import transform_dataframe
 
 
-def one_hot_encode_column(dataframe, column_name):
-    """ """
+def one_hot_encode_column(dataframe: pd.DataFrame, column_name: str):
+    """Applies the one-hot enconding method to a specified categorical
+    column of a given dataframe.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        Dataframe with the objective column.
+    column_name : str
+        Column name to be encoded
+
+    Returns
+    -------
+    pd.DataFrame
+        Return the input dataframe with the on-hot encoded columns added.
+    """
     return pd.concat(
         [
             dataframe,
@@ -18,8 +32,21 @@ def one_hot_encode_column(dataframe, column_name):
     ).drop(columns=column_name)
 
 
-def preprocess_transform(X, preprocess):
-    """ """
+def preprocess_transform(X: pd.DataFrame, preprocess: list):
+    """Applies a sequence of transformations in a given dataframe.
+
+    Parameters
+    ----------
+    X : pd.DataFrame
+        Dataframe to be transformed.
+    preprocess : list
+        List of sklearn type transformations to be applied on dataframe
+
+    Returns
+    -------
+    pd.DataFrame
+        Returns the transformed dataframe
+    """
 
     for p in preprocess:
         X = transform_dataframe(p, X)
@@ -27,8 +54,20 @@ def preprocess_transform(X, preprocess):
     return X
 
 
-def fit_preprocess_revenue_model_q2(X_train, y_train=None):
-    """ """
+def fit_preprocess_price_model_q1(X_train: pd.DataFrame):
+    """Train the preprocess pipeline stages of the model for the question 1
+    with a given dataframe
+
+    Parameters
+    ----------
+    X_train : pd.DataFrame
+        Dataframe to train the preprocess pipeline
+
+    Returns
+    -------
+    tuple
+        Returns a tuple with all stages of the pipeline trained.
+    """
     imputer = SimpleImputer(strategy="mean")
     imputer.fit(X_train)
     X_train = imputer.transform(X_train)
@@ -40,8 +79,20 @@ def fit_preprocess_revenue_model_q2(X_train, y_train=None):
     return imputer, scaler
 
 
-def fit_preprocess_price_model_q1(X_train, y_train=None):
-    """ """
+def fit_preprocess_revenue_model_q2(X_train: pd.DataFrame):
+    """Train the preprocess pipeline stages of the model for the question 2
+    with a given dataframe
+
+    Parameters
+    ----------
+    X_train : pd.DataFrame
+        Dataframe to train the preprocess pipeline
+
+    Returns
+    -------
+    tuple
+        Returns a tuple with all stages of the pipeline trained.
+    """
     imputer = SimpleImputer(strategy="mean")
     imputer.fit(X_train)
     X_train = imputer.transform(X_train)
@@ -53,8 +104,20 @@ def fit_preprocess_price_model_q1(X_train, y_train=None):
     return imputer, scaler
 
 
-def fit_preprocess_reservations_model_q3(X_train, y_train=None):
-    """ """
+def fit_preprocess_reservations_model_q3(X_train: pd.DataFrame):
+    """Train the preprocess pipeline stages of the model for the question 3
+    with a given dataframe
+
+    Parameters
+    ----------
+    X_train : pd.DataFrame
+        Dataframe to train the preprocess pipeline
+
+    Returns
+    -------
+    tuple
+        Returns a tuple with all stages of the pipeline trained.
+    """
     imputer = SimpleImputer(strategy="mean")
     imputer.fit(X_train)
     X_train = imputer.transform(X_train)
